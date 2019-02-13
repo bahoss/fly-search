685ed8183ab34a14178fd2c100abc1855d157917
bahoss <bahobek@gmail.com>
2019-02-13 22:08:27 +0600

    create project

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.styled.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <header>Fly search</header>
+      </div>
+    );
+  }
+}
+
+export default App;
