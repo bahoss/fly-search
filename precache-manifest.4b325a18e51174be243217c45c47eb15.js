83e7f1d0d8430b03ab2442df51397da8b1544fdd
bahoss <bahobek@gmail.com>
2019-02-14 01:06:39 +0600

    Updates

---
+++ precache-manifest.4b325a18e51174be243217c45c47eb15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a38e86e9bb6131c638b",
+    "url": "/search-page/static/css/main.21c99cfd.chunk.css"
+  },
+  {
+    "revision": "1a38e86e9bb6131c638b",
+    "url": "/search-page/static/js/main.1a38e86e.chunk.js"
+  },
+  {
+    "revision": "145aa177eb493680834b",
+    "url": "/search-page/static/js/runtime~main.145aa177.js"
+  },
+  {
+    "revision": "b54d9498ccf7cd25e1b9",
+    "url": "/search-page/static/js/2.b54d9498.chunk.js"
+  },
+  {
+    "revision": "d6d2680aa28690569b33433f21436701",
+    "url": "/search-page/index.html"
+  }
+];
